bc1afc80a10610fd3af13ba224d594b697e148bf
Lucas Sekikawa <lucassekikawa82@gmail.com>
2025-09-15 17:06:07 -0300

    Update Pesquisa_Sequencial.c

--- Algoritmos_Estrutura_de_Dados_II/Busca_e_Ordenação/Pesquisa_Sequencial.c
+++ Algoritmos_Estrutura_de_Dados_II/Busca_e_Ordenação/Pesquisa_Sequencial.c
@@ -1,13 +1,25 @@
 /* Busca por elemento em um vetor usando sentinela
-   Retorna 0 caso o elemento procurado não esteja no vetor
+   Retorna -1 caso o elemento procurado não esteja no vetor
    Caso contrário, retorna a posição do elemento */
 
-int pesquisaSeq (vetor[], int i, elementoProcurado)
+int pesquisaSeqSentinela (vetor[], int i, elementoProcurado)
 {
   vetor[0] = elementoProcurado;
   while (vetor[i] != elementoProcurado)
     i--;
   if (i == 0)
-      return 0;
+      return -1;
   return i;  
+}
+
+/* Busca por elemento em um vetor sem sentinela
+   Retorna -1 caso o elemento procurado não esteja no vetor
+   Caso contrário, retorna a posição do elemento */
+int pesquisaSeq (vetor [], int i, elementoProcurado)
+{
+   while ( (i >= 0) && (vetor[i] != elementoProcurado) )
+      i--;
+   if (i < 0)
+      return -1;
+   return i
 }
