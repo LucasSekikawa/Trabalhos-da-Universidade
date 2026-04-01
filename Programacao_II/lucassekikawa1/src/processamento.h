31aba9797226021729db0d38f675ab9100eb6d0c
Lucas Sekikawa <lucassekikawa82@gmail.com>
2026-04-01 09:19:48 -0300

    mais coisa

--- Programacao_II/lucassekikawa1/src/processamento.h
+++ Programacao_II/lucassekikawa1/src/processamento.h
@@ -3,6 +3,15 @@
 
 #include "estruturas.h"
 
+typedef void (ModoAnaliseFn)(char **arquivos, int num_arqs, int ini, int fim, int jan, double cap);
+
+
+/* Caso nao haja -w na entrada do programa, essa sera a funcao chamada, ela é a estrutura principal
+   do caso de calculo do rendimento final. 
+   Recebe o vetor de arquivos, seu tamanho, os anos de inicio e fim, e o capital inicial
+   Fará todo o processo, desde chamada de funcoes auxiliares para realizar os calculos, quanto
+   as impressoes e liberacao de memoria. */
+
 void modo_comparacao(char **arquivos, int num_arqs, int ini, int fim, int jan, double cap);
 
 /* Caso haja -w na entrada do programa, essa sera a funcao chamada, ela é a estrutura principal
